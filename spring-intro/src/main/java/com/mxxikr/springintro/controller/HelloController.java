a0d379d9e045f07407e98c2707c7af6c22d9db4f
mxxikr <mxxikr@gmail.com>
2024-03-30 11:37:52 +0900

    정적 컨텐츠와 mvc, 템플릿 엔진

--- spring-intro/src/main/java/com/mxxikr/springintro/controller/HelloController.java
+++ spring-intro/src/main/java/com/mxxikr/springintro/controller/HelloController.java
@@ -1,8 +1,10 @@
 package com.mxxikr.springintro.controller;
 
+import org.springframework.boot.Banner;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HelloController {
@@ -10,5 +12,11 @@ public class HelloController {
     public String hello(Model model) {
         model.addAttribute("data", "hello!");
         return "hello";
+    }
+
+    @GetMapping("hello-mvc")
+    public String helloMvc(@RequestParam(value = "name", required = false) String name, Model model) {
+        model.addAttribute("name", name);
+        return "hello-template";
     }
 }
