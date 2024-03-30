a7283fbdad21817f41b44272f84a32b0a4cd191c
mxxikr <mxxikr@gmail.com>
2024-03-30 13:52:03 +0900

    회원 관리 예제 - 회원 도메인과 리포지토리 생성

---
+++ spring-intro/src/main/java/com/mxxikr/springintro/repository/MemberRepository.java
@@ -0,0 +1,13 @@
+package com.mxxikr.springintro.repository;
+
+import com.mxxikr.springintro.domain.Member;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface MemberRepository {
+    Member save(Member member);
+    Optional<Member> findById(Long id);
+    Optional<Member> findByName(String name);
+    List<Member> findAll();
+}
