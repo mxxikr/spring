e7d45ddec0562e58cba83f7052e45116e8e1ccd6
mxxikr <mxxikr@gmail.com>
2024-04-07 07:17:42 +0900

    회원 서비스 개발 - 회원 가입 로직 리팩토링

--- spring-intro/src/main/java/com/mxxikr/springintro/service/MemberService.java
+++ spring-intro/src/main/java/com/mxxikr/springintro/service/MemberService.java
@@ -2,22 +2,26 @@ package com.mxxikr.springintro.service;
 
 import com.mxxikr.springintro.domain.Member;
 import com.mxxikr.springintro.repository.MemberRepository;
+import com.mxxikr.springintro.repository.MemoryMemberRepository;
 
 import java.util.Optional;
 
 public class MemberService {
-    private final MemberRepository memberRepository = new MemberRepository();
+    private final MemberRepository memberRepository = new MemoryMemberRepository();
 
     /**
      * 회원 가입
      */
     public Long join(Member member) {
-        // 같은 이름을 가진 회원은 중복 회원으로 가입 불가
-        Optional<Member> result = memberRepository.findByName(member.getName());    
-        result.ifPresent(m -> {
-            throw new IllegalStateException("이미 존재하는 회원입니다.");
-        });
+        validateDuplicateMember(member); // 중복 회원 검증
         memberRepository.save(member);
         return member.getId();
+    }
+
+    public void validateDuplicateMember(Member member) {
+        memberRepository.findByName(member.getName())
+                .ifPresent(m -> {
+                    throw new IllegalStateException("이미 존재하는 회원입니다.");
+                });
     }
 }
